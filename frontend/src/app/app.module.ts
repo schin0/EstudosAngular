bb43b26cd428a087b56786277c74cd9574f0b07b
Gabriel Schincariol <gabriel.schincariol@iv2.com.br>
2022-01-05 20:36:46 -0300

    Arquivos de componentes que ocupam a tela

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,18 +9,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
 import { NavComponent } from './components/template/nav/nav.component';
 import { FooterComponent } from './components/template/footer/footer.component';
+import { InicioComponent } from './views/inicio/inicio.component';
 
 // Material:
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +31,8 @@ import { MatListModule } from '@angular/material/list';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
