833f34f656b2f147eacbd5105485327c986d6139
Gabriel Schincariol <gabriel.schincariol@iv2.com.br>
2022-01-11 21:21:34 -0300

    Rotas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -23,6 +25,14 @@ const routes: Routes = [
   {
     path: "products/criar",
     component: ProductCreateComponent
+  },
+  {
+    path: "products/editar/:id",
+    component: ProductUpdateComponent
+  },
+  {
+    path: "products/excluir/:id",
+    component: ProductDeleteComponent
   },
   {
     path: "products/visualizar/:id",
