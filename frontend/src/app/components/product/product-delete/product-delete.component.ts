6f5775428007c070d7979a9b6876050b36a74ebd
Gabriel Schincariol <gabriel.schincariol@iv2.com.br>
2022-01-11 21:22:08 -0300

    Criação tela para excluir produto

---
+++ frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -0,0 +1,41 @@
+import { MensagemService } from './../shared/mensagem.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from './../shared/product.service';
+import { Product } from './../shared/product.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-delete',
+  templateUrl: './product-delete.component.html',
+  styleUrls: ['./product-delete.component.scss']
+})
+export class ProductDeleteComponent implements OnInit {
+
+  product: Product = { name: '', price: 0 };
+  id: number = 0;
+
+  constructor(
+    private productService: ProductService,
+    private mensagem: MensagemService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params["id"];
+    this.productService.lerPeloId(this.id).subscribe(product => {
+      this.product = product;
+    });
+  }
+
+  deleteProduto(): void {
+    this.productService.excluir(this.id).subscribe(() => {
+      this.mensagem.mostrar('Produto excluído com sucesso!');
+      this.router.navigate(['/products']);
+    })
+  }
+
+  cancelar(): void {
+    this.router.navigate(['/products']);
+  }
+}
